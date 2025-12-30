e2f5a73d992fc09f6c090f1e86833ed20336d799
maradadivyasree020 <maradadivyasree@gmail.com>
2025-12-30 11:53:08 +0530

    commit

--- src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
+++ src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
@@ -3,19 +3,28 @@ package com.example.college.pipeline;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.college.CollegeApplication;
+import com.example.college.controller.AttendanceController;
+import com.example.college.controller.EmployeeController;
+import com.example.college.controller.TestCaseController;
+import com.example.college.service.AttendanceService;
 
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = {AttendanceController.class,EmployeeController.class,TestCaseController.class})
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class JsonDrivenTestRunnerTest {
 
     @Autowired
     private TestExecutionPipeline pipeline;
+
+    @MockBean
+    private AttendanceService attendanceService;
 
     @Test
     void runJsonDrivenTestsAndGeneratePdf() throws Exception {
