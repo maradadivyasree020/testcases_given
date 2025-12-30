dcda75aa14683c5d7310ef87a34e925a761cb53b
maradadivyasree020 <maradadivyasree@gmail.com>
2025-12-30 10:11:35 +0530

    Changed jdk version back to spring boot test

--- src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
+++ src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
@@ -1,34 +1,17 @@
-// package com.example.college.pipeline;
-
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-// import org.springframework.boot.test.context.SpringBootTest;
-
-// @SpringBootTest
-// @AutoConfigureMockMvc
-// class JsonDrivenTestRunnerTest {
-
-//     @Autowired
-//     private TestExecutionPipeline pipeline;
-
-//     @Test
-//     void runJsonDrivenTestsAndGeneratePdf() throws Exception {
-//         var results = pipeline.executeAllTests();
-//         PdfReportGenerator.generate(results);
-//     }
-// }
-
-
 package com.example.college.pipeline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@WebMvcTest
+import com.example.college.CollegeApplication;
+
+
+@SpringBootTest(classes = CollegeApplication.class,webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class JsonDrivenTestRunnerTest {
 
     @Autowired
