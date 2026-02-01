4ce98f6dd551950fb194afec58542f68c1e9e241
maradadivyasree020 <maradadivyasree@gmail.com>
2026-02-01 21:00:18 +0530

    changed test runner

--- src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
+++ src/test/java/com/example/college/pipeline/JsonDrivenTestRunnerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@ActiveProfiles("test")                 
+@AutoConfigureMockMvc(addFilters = false) 
 class JsonDrivenTestRunnerTest {
 
     @Autowired
